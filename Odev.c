92531d15efc8c58cce58c0e74a7caa02d44e5076
HuseyinCanCelikkol <48149182+HuseyinCanCelikkol@users.noreply.github.com>
2019-04-20 19:29:02 +0300

    ReadTeamNames Kodladım
    ReadTeamNames kodladım. 
    
    structa depoluyor. (lname ve sname)

--- Odev.c
+++ Odev.c
@@ -15,7 +15,25 @@ struct team{
     uint points;
 };
 
-char ReadTeamNames(); //takimlar.txt icinden takimlarin isimlerini okumayi saglayan fonksiyon.
+void ReadTeamNames()//takimlar.txt icinden takimlarin isimlerini okumayi saglayan fonksiyon.
+{
+	int n;
+	int i;
+	FILE *fp;
+	fp = fopen("C:/takimlar.txt","r");//konum değiştirilecek. ben kolaylık olsun diye c nin içerisine attım.
+
+	printf("Kac takim olacak? :  ");
+	scanf("%d",&n);
+	
+	struct team teams[n];//n tane takımın verilerini buradan saklıyorum.
+
+	for(i=0;i<n;i++)
+	fscanf(fp,"%s %s",teams[i].lname,teams[i].sname);	
+	
+
+	fclose(fp);
+} 
+
 void ReadMatches(); //maclar.1txt dosyasindan maclari okumayi saglayan fonksiyon.
 
 int main(){
