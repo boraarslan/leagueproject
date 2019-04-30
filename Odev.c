98a6382f7ac769f973a7554806cb5fbc9251e5a2
HuseyinCanCelikkol <48149182+HuseyinCanCelikkol@users.noreply.github.com>
2019-04-30 16:09:23 +0300

    Update Odev.c
    readmatch
    readteams

--- Odev.c
+++ Odev.c
@@ -17,20 +17,34 @@ struct team{
 
 void ReadTeamNames()//takimlar.txt icinden takimlarin isimlerini okumayi saglayan fonksiyon.
 {
-	int n;//bunu global tanimlamamiz gerek. takim sayisini buradan seciyoruz.
+	
 	int i;
+	int n;
 	FILE *fp;
 	fp = fopen("C:/takimlar.txt","r");//konum değiştirilecek. ben kolaylık olsun diye c nin içerisine attım.
-
-	printf("Kac takim olacak? :  ");
+	
+	printf("kac takim okunacak: ");
 	scanf("%d",&n);
+	struct team teams[n];
+	if(n>10){
+		printf("Fazla sayi girdiniz.Onun yerine tum takimlar gosteriliyor ve depolaniyor.\n\n");
+		for(i=0;i<10;i++){	
+			fscanf(fp,"%s",&teams[i].sname);
+			printf("%s ",teams[i].sname);	
+			fscanf(fp,"%s",&teams[i].lname);
+			printf("%s \n",teams[i].lname);
+		}
+	}
 	
-	struct team teams[n];//n tane takımın verilerini buradan saklıyorum.
-
-	for(i=0;i<n;i++)
-	fscanf(fp,"%s %s",teams[i].lname,teams[i].sname);	
-	
-
+	else
+	{
+		for(i=0;i<n;i++){
+			fscanf(fp,"%s",&teams[i].sname);
+			printf("%s ",teams[i].sname);	
+			fscanf(fp,"%s",&teams[i].lname);
+			printf("%s \n",teams[i].lname);
+		}
+	}
 	fclose(fp);
 } 
 
@@ -40,36 +54,55 @@ struct match{
 	int ascore;
 	char ateam;
 };
+
 void ReadMatches()
 {
 	int i;
 	int m;//mac sayisi
+	int n;
 	
 	FILE *fp;
-	fp = fopen("C:/maclar1.txt","r");//değiştirilecek konum.
+	fp = fopen("C:/maclar1.txt","r");//KONUM DEĞİŞTİRİLECEK.
 	
 	printf("Dosyadan Kac mac okunacak?: ");
 	scanf("%d",&m);
-	
 	struct match matches[m];
-	
 	if(fp){
-		if(!feof(fp)){
-		
+		if(m>12){
+			printf("Diger %d maci giriniz(Takim kisaltmalarini buyuk harf giriniz): ",m-12);
+			for(i=12;i<m;i++){
+				scanf(" %c %d %c %d",&matches[i].hteam,&matches[i].hscore,&matches[i].ateam,&matches[i].ascore);
+				printf("Bu maci girdiniz: %c %d %c %d\n",matches[i].hteam,matches[i].hscore,matches[i].ateam,matches[i].ascore);
+			}
+			
+			printf("\n\n-------------------------\n\n");
+			
+			for(i=0;i<12;i++){
+				fscanf(fp,"%c %d %c %d\n",&matches[i].hteam,&matches[i].hscore,&matches[i].ateam,&matches[i].ascore);
+				printf("%d. mac:  %c %d %c %d\n",i+1,matches[i].hteam,matches[i].hscore,matches[i].ateam,matches[i].ascore);
+			}
+			
+			printf("\nIlaveten eklenen maclar:\n\n");
+			
+			for(i=12;i<m;i++){
+				printf("%d. mac:  %c %d %c %d\n",i+1,matches[i].hteam,matches[i].hscore,matches[i].ateam,matches[i].ascore);
+			}
+			
+		}
+		else if(m<12){
 			for(i=0;i<m;i++)
 			{	
-			fscanf(fp,"%c %d %c %d\n",&matches[i].hteam,&matches[i].hscore,&matches[i].ateam,&matches[i].ascore);
-			printf("%d. mac:  %c %d %c %d\n",i+1,matches[i].hteam,matches[i].hscore,matches[i].ateam,matches[i].ascore);
+				fscanf(fp,"%c %d %c %d\n",&matches[i].hteam,&matches[i].hscore,&matches[i].ateam,&matches[i].ascore);
+				printf("%d. mac:  %c %d %c %d\n",i+1,matches[i].hteam,matches[i].hscore,matches[i].ateam,matches[i].ascore);
 			}
 		}
 	}
-
-	else{
-	
-		printf("dosya okunamadi!");
 		
+	
+	else{
+		printf("dosya okunamadi!");
 	}
-	fclose(fp);
+fclose(fp);
 }
 
 int main(){
