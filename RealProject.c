58149a8b5da03dbd9f44c40ba3262acf9c95e87d
boraarslan <44371603+boraarslan@users.noreply.github.com>
2019-04-30 16:07:14 +0300

    Update RealProject.c

--- RealProject.c
+++ RealProject.c
@@ -12,76 +12,108 @@ typedef struct teams{
     uint nscore;
     int average;
     int points;
-    char* mchart;   //
-}Team;
+    char* mchart;
+} Team;
 
-int NumberOfTeams;                                                  //Takim sayisini tutan degisken
-int* AddressOfNumberOfTeams = &NumberOfTeams;                       //Takim sayisinin pointeri
-Team* AdressToStartOfTeams;
-
-void CopyString(char from[] , char to[]){
+int NumberOfTeams;                                      //Takim sayisini tutan degisken
+int *AddressOfNumberOfTeams = &NumberOfTeams;           //Takim sayisinin pointeri
+Team *AdressToStartOfTeams;
+int* PointArray;
+int PointsForWin = 3;
+int PointsForLose = -1;
+int PointsForDraw = 1;
+void CopyString(char from[], char to[]){
     uint i = 0;
-    for(;from[i] != '\0';i++){
+    for (; from[i] != '\0'; i++){
         to[i] = from[i];
     }
     to[i] = '\0';
 }
 
-Team* MoveStruct(int shift){
+Team *MoveStruct(int shift){
     return (AdressToStartOfTeams + shift);
 }
 
 void AllocateMemory(int TeamCount){
-    AdressToStartOfTeams = calloc(sizeof(Team) , TeamCount);
-    for(int i = 0; i <= TeamCount ; i++){
-        MoveStruct(i)->mchart = (char*)calloc(sizeof(char) , TeamCount);
+    AdressToStartOfTeams = calloc(sizeof(Team), TeamCount);
+    for (int i = 0; i <= TeamCount; i++){
+        MoveStruct(i)->mchart = (char *)calloc(sizeof(char), TeamCount);
     }
+    PointArray = calloc(sizeof(int) , TeamCount);
 }
 
-void ReadTeamNumberFromConsole(int* address){                                //Takim sayisi verisini almaak icin ornek fonksiyon
+void ReadTeamNumberFromConsole(int *address){                       //Takim sayisi verisini almaak icin ornek fonksiyon
     printf("Kac tane takim olacak?");
-    scanf("%d",address);
+    scanf("%d", address);
 }
 
 void ReadMatchFromKeyboard(){
-    printf("Maclari tek tek girin cikmak icin 33 yazin.");
-    char HomeTeam;
-    int HomeTeamScore;
-    char GuestTeam;
-    int GuestTeamScore;
-    while(1){
-        scanf("%c",&HomeTeam);
-        if(HomeTeam == '!'){
-            printf("naw");
+    printf("Maclari tek tek girin cikmak icin ! yazin.");
+    char HomeTeam = 0;
+    int HomeTeamScore = 0;
+    char GuestTeam = 0;
+    int GuestTeamScore = 0;
+    while (1){
+        scanf("%c", &HomeTeam);
+        if (HomeTeam == '!'){
             break;
         }
 
-        scanf("%d %c %d", &HomeTeamScore , &GuestTeam , &GuestTeamScore);
+        scanf("%d %c %d", &HomeTeamScore, &GuestTeam, &GuestTeamScore);
 
-        for(int i = 0;i <= NumberOfTeams; i++){
+        if (((int)(HomeTeam)-65) >= NumberOfTeams || ((int)(GuestTeam)-65) >= NumberOfTeams){
+            printf("Lutfen gecerli takim adi girin.\n");
+            HomeTeam = 0;
+            GuestTeam = 0;
+            continue;
+        }
 
-            if(HomeTeam == MoveStruct(i)->sname){
+        for (int i = 0; i < NumberOfTeams; i++){
 
-                for(int a = 0; a <= NumberOfTeams; a++){
+            if (HomeTeam == MoveStruct(i)->sname){                                  //Structlar icinden takimi buluyor
 
-                    if(GuestTeam == MoveStruct(a)->sname){
-                        MoveStruct(i)->pscore += HomeTeamScore;
-                        MoveStruct(i)->nscore += GuestTeamScore;
-                        MoveStruct(a)->pscore += GuestTeamScore;
+                for (int a = 0; a < NumberOfTeams; a++){
+
+                    if (GuestTeam == MoveStruct(a)->sname){                         //Structlar icinden takimi buluyor
+
+                        if (a == i){
+                            printf("Ayni takim kendiyle mac yapamaz.\n");
+                            break;
+                        }
+
+                        if (MoveStruct(i)->mchart[a] == '\0'){
+                            printf("Bu ev sahibi takim bu deplasmandaki takimla zaten mac yapmis.\n");
+                            break;
+                        }
+
+                        MoveStruct(i)->mchart[a] = '\0';
+
+                        MoveStruct(i)->pscore += HomeTeamScore;                     //Kodun bu kisimi atilan ve
+                        MoveStruct(i)->nscore += GuestTeamScore;                    //yenilen gollerin structlara
+                        MoveStruct(a)->pscore += GuestTeamScore;                    //veri olarak girilmesini sagliyor
                         MoveStruct(a)->nscore += HomeTeamScore;
                         MoveStruct(i)->average += HomeTeamScore - GuestTeamScore;
                         MoveStruct(a)->average += GuestTeamScore - HomeTeamScore;
-                        if(HomeTeamScore > GuestTeamScore){
+
+                        if (HomeTeamScore > GuestTeamScore){                        //Ev sahibi kazanirsa
                             MoveStruct(i)->won += 1;
                             MoveStruct(a)->lost += 1;
+                            MoveStruct(i)->points += PointsForWin;
+                            MoveStruct(a)->points += PointsForLose;
                         }
-                        if(HomeTeamScore < GuestTeamScore){
+
+                        if (HomeTeamScore < GuestTeamScore){                        //Deplasman takimi kazanirsa
                             MoveStruct(a)->won += 1;
                             MoveStruct(i)->lost += 1;
+                            MoveStruct(a)->points += PointsForWin;
+                            MoveStruct(i)->points += PointsForLose;
                         }
-                        if(HomeTeamScore == GuestTeamScore){
+
+                        if (HomeTeamScore == GuestTeamScore){                       //Berabere kalinirsa
                             MoveStruct(i)->draw += 1;
                             MoveStruct(a)->draw += 1;
+                            MoveStruct(i)->points += PointsForDraw;
+                            MoveStruct(a)->points += PointsForDraw;
                         }
                     }
                 }
@@ -91,9 +123,9 @@ void ReadMatchFromKeyboard(){
 }
 
 void BuildTeams(int TeamCount){
-    for(struct {char i; int a;}  fV = {65 , 0};
+    for (struct{char i; int a; } fV ={65, 0};
                                 fV.a < TeamCount;
-                                fV.a++ , fV.i++){
+                                fV.a++, fV.i++){
         MoveStruct(fV.a)->sname = fV.i;
         MoveStruct(fV.a)->points = 0;
         MoveStruct(fV.a)->played = 0;
@@ -104,18 +136,54 @@ void BuildTeams(int TeamCount){
         MoveStruct(fV.a)->pscore = 0;
         MoveStruct(fV.a)->nscore = 0;
         MoveStruct(fV.a)->average = 0;
-        for(int i = 0; i <= TeamCount; i++){
-            MoveStruct(fV.a)->mchart[i] = 65+i;
+        for (int i = 0; i <= TeamCount; i++){
+            MoveStruct(fV.a)->mchart[i] = 65 + i;
+        }
+        MoveStruct(fV.a)->mchart[fV.a] = '\0';
+    }
+    for(int i = 0; i < NumberOfTeams; i++){
+        PointArray[i] = i;
+    }
+    
+}
+
+void SortTeamsByPoint(){
+    for(int i = 0; i < NumberOfTeams; i++){
+        for(int a = i; a > 0 ; a--){
+            int temp;
+            if(MoveStruct(PointArray[a])->points > MoveStruct(PointArray[a-1])->points){
+                temp = PointArray[a-1];
+                PointArray[a-1] = PointArray[a];
+                PointArray[a] = temp;
+            }
         }
     }
 }
 
-
+void PrintTableByPoint(){
+    for(int i = 0; i < NumberOfTeams; i++){
+        printf("%c: %d\n", MoveStruct(PointArray[i])->sname , MoveStruct(PointArray[i])->points);
+    }
+}
 
 int main(){
     ReadTeamNumberFromConsole(AddressOfNumberOfTeams);
     AllocateMemory(NumberOfTeams);
     BuildTeams(NumberOfTeams);
+    for (int i = 0; i < NumberOfTeams; i++){
+        for (int a = 0; a < NumberOfTeams; a++){
+            printf("%c ", MoveStruct(i)->mchart[a]);
+        }
+        printf("\n");
+    }
     ReadMatchFromKeyboard();
+    for (int i = 0; i < NumberOfTeams; i++){
+        for (int a = 0; a < NumberOfTeams; a++){
+            printf("%c ", MoveStruct(i)->mchart[a]);
+        }
+        printf("\n");
+    }
+    SortTeamsByPoint();
+    PrintTableByPoint();
     return 0;
 }
